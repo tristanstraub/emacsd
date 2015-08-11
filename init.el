427a9031a4cf0e9898be5e3b271e568ccfaabf76
Tristan Straub <tstraub@locomote.com>
2015-08-11 16:15:26 +1000

    Modeline face changes

--- init.el
+++ init.el
@@ -67,4 +67,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 128 :width normal)))))
+ '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 128 :width normal))))
+ '(mode-line ((t (:background "saddle brown" :foreground "gainsboro" :box (:line-width 1 :style released-button)))))
+ '(mode-line-buffer-id ((t (:foreground "white" :weight bold)))))

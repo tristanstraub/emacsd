4475dd1f5dac8509ab6fac990315854f7e7edb4a
Tristan Straub <tristanstraub@gmail.com>
2015-09-28 19:26:20 +1000

    Various things

--- init.el
+++ init.el
@@ -62,13 +62,15 @@
            (whitespace-mode 0)
            (whitespace-mode 1))
      (whitespace-line-column . 80)
-     (whitespace-style face tabs trailing lines-tail)))))
+     (whitespace-style face tabs trailing lines-tail))))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 128 :width normal))))
+ '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 143 :width normal))))
  '(mode-line ((t (:background "saddle brown" :foreground "gainsboro" :box (:line-width 1 :style released-button)))))
  '(mode-line-buffer-id ((t (:foreground "white" :weight bold)))))
 (put 'narrow-to-region 'disabled nil)
